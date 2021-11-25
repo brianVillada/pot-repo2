7b060817800999fab87fea1f875c8904809e4d48
Brian Villada <brian.villada@udea.edu.co>
2021-11-24 22:47:36 -0500

    next app with tailwind

---
+++ pot-app/pages/usuarios/brian.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const brian = () => {
+    return (
+        <div>
+            ir a otra pagina 
+            <p>
+                sentido
+            </p>
+        </div>
+    )
+}
+
+export default brian
